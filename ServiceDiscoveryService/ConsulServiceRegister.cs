92c201447b5ffe9455fa2bcd74258dba20e81b54
Val <valeriu.viscreanu@gmail.com>
2023-02-08 21:38:51 +0200

    Updated consul configuration for K8S

--- ServiceDiscoveryService/ConsulServiceRegister.cs
+++ ServiceDiscoveryService/ConsulServiceRegister.cs
@@ -15,22 +15,31 @@ internal class ConsulServiceRegister : IHostedService, IConsulServiceRegister
         this.platformCfg = platformCfg.Value;
         this.client = client;
     }
-public async Task StartAsync(CancellationToken cancellationToken)
-{
-
-    var platformUri = new Uri(platformCfg.Url);
-    var agentServiceRegistration = new AgentServiceRegistration() 
+    public async Task StartAsync(CancellationToken cancellationToken)
     {
-        Address = platformUri.Host,
-        Port = platformUri.Port,
-        Name = platformCfg.ServiceName,
-        ID = platformCfg.ServiceId
 
-    };
-    
-    await client.Agent.ServiceDeregister(platformCfg.ServiceId, cancellationToken);
-    await client.Agent.ServiceRegister(agentServiceRegistration, cancellationToken);    
-}
+        var platformUri = new Uri(platformCfg.Url);
+        var agentServiceRegistration = new AgentServiceRegistration()
+        {
+            Address = platformUri.Host,
+            Port = platformUri.Port,
+            Name = platformCfg.ServiceName,
+            ID = platformCfg.ServiceId
+
+        };
+        try
+        {
+            Console.WriteLine($"--> Starting to register {agentServiceRegistration.Address}");
+            await client.Agent.ServiceDeregister(platformCfg.ServiceId, cancellationToken);
+            await client.Agent.ServiceRegister(agentServiceRegistration, cancellationToken);
+            var r = await client.Agent.ServiceRegister(agentServiceRegistration, cancellationToken);
+            Console.WriteLine($" Service registered status:{r.StatusCode}");
+        }
+        catch (System.Net.Http.HttpRequestException ex)
+        {
+            Console.WriteLine($"Connection err occured on consul: {ex}");
+        }
+    }
 
     public async Task StopAsync(CancellationToken cancellationToken)
     {
